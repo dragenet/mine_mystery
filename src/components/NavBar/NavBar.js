3c0b256dbcd78ef76198813137455eb4b8c6f48b
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2020-08-30 17:18:46 +0200

    NavBar
    Add logo image
    Add app name

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,13 @@ import React from "react";
 
 import styles from "./NavBar.module.scss";
 
-const NavBar = () => <div className={styles.wrapper}></div>;
+import logo from "../../assets/icons/mine-wagon.svg";
+
+const NavBar = () => (
+  <div className={styles.wrapper}>
+    <img className={styles.logoImage} src={logo} alt="mine wagon" />
+    <h1 className={styles.name}>Mine Mystery</h1>
+  </div>
+);
 
 export default NavBar;
