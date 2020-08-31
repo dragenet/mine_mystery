8d85f3f29a5d49b5605b46d358c18a7aedb59bdc
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2020-08-31 11:01:03 +0200

    Add container for main content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 
+import styles from "./App.module.scss";
+
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 class App extends React.Component {
+  state = {};
+
   render() {
     return (
       <>
         <NavBar />
-        <h1>Hello world</h1>
+        <div className={styles.content}>
+          <h1>Hello world</h1>
+        </div>
+        <Footer />
       </>
     );
   }
