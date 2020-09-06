46e5f613be057e9666a46dd63425195147f1c6f4
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2020-09-06 16:57:47 +0200

    Add firebase functions as API

---
+++ firebase/functions/index.js
@@ -0,0 +1,43 @@
+const functions = require("firebase-functions");
+const omit = require("lodash.omit");
+
+const firebase = require("firebase-admin");
+firebase.initializeApp();
+
+const db = firebase.firestore();
+
+const collectionName = "questions";
+
+exports.getRandom = functions.https.onRequest(async (request, response) => {
+  await db
+    .collection(collectionName)
+    .get()
+    .then((res) => {
+      var pack_data = [];
+      console.log(res);
+      res.forEach((datax) => {
+        pack_data.push({ id: datax.id });
+      });
+      for (var x = 0; x < pack_data.length; x++) {
+        var randomData = Math.floor(Math.random() * pack_data.length + 1);
+        db.collection(collectionName)
+          .doc(pack_data[x].id)
+          .update({ random: randomData });
+      }
+      return;
+    })
+    .catch((err) => {
+      return response.status(404).send("ERROR " + err);
+    });
+
+  const arr = [];
+  const result = await db
+    .collection(collectionName)
+    .orderBy("random", "desc")
+    .limit(3)
+    .get();
+  result.forEach((res) => {
+    arr.push(Object.assign({ id: res.id }, omit(res.data(), "random")));
+  });
+  return response.send(arr);
+});
